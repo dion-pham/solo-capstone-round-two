d5a923f915c67a481915d5ac6f1e8a15c5aec14b
Dion Pham <dinhanpham310@gmail.com>
2022-11-28 18:01:33 -0800

    dynamic pricing and subtotal in cart finished

--- react-app/src/components/Cart/index.js
+++ react-app/src/components/Cart/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { ADD_TO_CART } from "../../store/cart";
+import cartReducer, { ADD_TO_CART } from "../../store/cart";
 
 const Cart = () => {
     const {cart} = useSelector(state=> state.cart)
@@ -25,50 +25,62 @@ const Cart = () => {
         })
     }
 
+    let cartRender;
+    cart.length ? cartRender = (cart.map((product) =>
+    <div key={product.id} className='cart-card'>
+        <Link className='cart-card-link' to={`/products/${product.id}`}>
+            <img src= {product.img_url1} alt="Product's image" className='card-splash-image'></img>
+        </Link>
+        <div>
+            <div>
+                {product.name}
+                {product.size}
+            </div>
+            <div>
+                <h2>Price</h2>
+                ${product.price}
+            </div>
+            <div>
+                <h2>Quantity</h2>
+                <input
+                    type='number'
+                    min = '1'
+                    max = '5'
+                    value={product.quantity}
+                    onChange = {e => changeCartQuantity(e,product)}
+                />
+            </div>
+            <div>
+                <h2>Total</h2>
+                ${product.price*product.quantity}
+            </div>
+        </div>
+    </div>
+        ))
+        : cartRender = (<h1>Your cart is currently empty.</h1>)
+
+    let cartSubtotal;
+    cart.length? cartSubtotal = (
+    <div>
+        <h1>
+            Subtotal
+            ${cart.reduce((sum, product) => sum + (product.quantity*product.price), 0)}
+        </h1>
+    </div>
+    )
+    : cartSubtotal = (<h2>Purchase something</h2>)
+
+
+
     return (
         <div>
+            <div>
+                {cartRender}
+            </div>
+            <div>
+                {cartSubtotal}
+            </div>
 
-            {cart.length ?
-            (cart.map((product) =>
-                    <div key={product.id} className='cart-card'>
-                        <Link className='cart-card-link' to={`/products/${product.id}`}>
-                        <img src= {product.img_url1} alt="Product's image" className='card-splash-image'></img>
-                        </Link>
-                        <div>
-                            <div>
-                                {product.name}
-                                {product.size}
-                            </div>
-                            <div>
-                                Price_
-                                ${product.price}
-                            </div>
-                            <div>
-                                Quantity_
-                                <input
-                                    type='number'
-                                    min = '1'
-                                    max = '5'
-                                    value={product.quantity}
-                                    onChange = {e => changeCartQuantity(e,product)}
-                                />
-
-                            </div>
-                            <div>
-                                Total_
-                                {product.price*product.quantity}
-                            </div>
-                        </div>
-                    </div>
-                )
-            )
-            :
-            (
-                <h1>
-                    Your cart is currently empty.
-                </h1>
-            )
-        }
         </div>
     )
 }
