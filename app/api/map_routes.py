72960714024e21e3b8848f5ef0c76a00669a6d73
Dion Pham <dinhanpham310@gmail.com>
2023-01-10 10:17:50 -0800

    finished post route for google maps api key

--- app/api/map_routes.py
+++ app/api/map_routes.py
@@ -2,7 +2,12 @@ from flask import Blueprint, jsonify
 from app.models import User, db
 from app.models.product import Product
 from flask_login import current_user, login_required
+from app.config import Config
+key = Config.googleMapsAPIKey
+
 
 map_routes = Blueprint('map', __name__)
 
 @map_routes.route('/key', methods=['POST'])
+def fetch_api_key():
+    return jsonify({'googleMapsAPIKey': key})
